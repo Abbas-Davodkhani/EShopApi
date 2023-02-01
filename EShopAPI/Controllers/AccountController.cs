4b3d4a8041294f9eafd3ec6afd66f13bf07801b9
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2023-02-01 17:28:57 +0330

    Config Jwt

---
+++ EShopAPI/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using EShopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController() { }
+        public IActionResult Login(Account account)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+            if (account.UserName != "abc" || account.Password != "abc") { return BadRequest("Invalid"); }
+
+            var secertKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MyEShopSecurityKey"));
+            var signInCredentials = new SigningCredentials(secertKey , SecurityAlgorithms.HmacSha256);
+
+            var tokenOption = new JwtSecurityToken(
+                issuer: "", 
+                claims: new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name , account.UserName) , 
+                    new Claim(ClaimTypes.Role , "admin")
+                },
+                expires:DateTime.Now.AddMinutes(30) ,
+                signingCredentials:signInCredentials
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOption);
+
+            return Ok(new {token = tokenString});
+        }
+    }
+}
--- EShopAPI/Program.cs
+++ EShopAPI/Program.cs
@@ -1,7 +1,10 @@
 using EshopApi.Models;
 using EShopAPI.Contracts;
 using EShopAPI.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,34 @@ builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddMemoryCache();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer= true,
+            ValidateAudience= false,
+            ValidateLifetime= true,
+            ValidateIssuerSigningKey= true,
+            ValidIssuer = "http://localhost:5162",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MyEShopSecurityKey"))
+        };
+    });
+;
+
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("EnableCors", builder =>
+    {
+        builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .Build();
+    });
+});
+
 
 var app = builder.Build();
 
@@ -26,6 +57,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("EnableCors");
+app.UseAuthentication();
 
 app.UseAuthorization();
 
